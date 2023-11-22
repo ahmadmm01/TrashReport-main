ba27145d345e6b27af26d705e12d72cd7f5d8130
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2023-11-22 13:44:39 +0700

    fix

--- Integration_test/signup_test.dart
+++ Integration_test/signup_test.dart
@@ -12,33 +12,26 @@ void main() {
     );
 
     final FirebaseAuth _auth = FirebaseAuth.instance;
-    // Build our app and trigger a frame.
+   
     await tester.pumpWidget(
       MaterialApp(
         home: AuthSignupScreen(),
       ),
     );
 
-    // Verify that the title is rendered.
     expect(find.text('Sign Up'), findsOneWidget);
 
-    // Enter text in Full Name field
     await tester.enterText(find.byKey(Key('fullNameField')), 'John Doe');
 
-    // Enter text in Email field
     await tester.enterText(
         find.byKey(Key('emailField')), 'john.doe@example.com');
 
-    // Enter text in Password field
     await tester.enterText(find.byKey(Key('passwordField')), 'password123');
 
-    // Tap on the Sign Up button
     await tester.tap(find.byKey(Key('signUpButton')));
 
-    // Wait for the animation to complete and navigate to the next screen
     await tester.pumpAndSettle();
 
-    // Verify that the new screen is rendered
     expect(find.text('Success'), findsOneWidget);
     expect(find.text('Register Success'), findsOneWidget);
   });
--- Integration_test/trash_test.dart
+++ Integration_test/trash_test.dart
@@ -12,56 +12,41 @@ void main() {
     );
 
     final FirebaseAuth _auth = FirebaseAuth.instance;
-    // Build our app and trigger a frame.
     await tester.pumpWidget(
       MaterialApp(
         home: TrashScreen(),
       ),
     );
 
-    // Verify that the title is rendered.
     expect(find.text('OMOTRASH'), findsOneWidget);
 
-    // Enter text in Name field
     await tester.enterText(find.byKey(Key('nameField')), 'John Doe');
 
-    // Enter text in Phone Number field
     await tester.enterText(find.byKey(Key('phoneNoField')), '081234567890');
 
-    // Tap on the Select Type dropdown
     await tester.tap(find.byKey(Key('typeDropdown')));
 
-    // Wait for the animation to complete and the dropdown to expand
     await tester.pumpAndSettle();
 
-    // Tap on the 'Can' option in the dropdown
     await tester.tap(find.text('Can'));
 
-    // Verify that the selected type is 'Can'
     expect(find.text('Can'), findsOneWidget);
 
-    // Enter text in Weight field
     await tester.enterText(find.byKey(Key('weightField')), '6.5 kg');
 
-    // Enter text in Address field
     await tester.enterText(find.byKey(Key('addressField')),
         'Urban Village, Sub-district, District');
 
-    // Enter text in Location Details field
     await tester.enterText(
         find.byKey(Key('locationField')), 'House/Unit/Floor number');
 
-    // Enter text in Landmark field
     await tester.enterText(
         find.byKey(Key('landmarkField')), 'Next to a coffee shop');
 
-    // Tap on the Submit button
     await tester.tap(find.byKey(Key('submitButton')));
 
-    // Wait for the animation to complete and navigate to the next screen
     await tester.pumpAndSettle();
 
-    // Verify that the success message is displayed
     expect(find.text('Success'), findsOneWidget);
     expect(find.text('Data saved successfully'), findsOneWidget);
   });
